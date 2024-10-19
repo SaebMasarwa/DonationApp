5a9aa448ec169fa4c3cf01b4d21f0e284c99d7eb
Saeb Masarwa <saeb2312@gmail.com>
2024-10-19 18:20:12 +0300

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json 	new file:   redux/reducers/User.js 	new file:   redux/store.js 	modified:   screens/Home/Home.js

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import {SafeAreaView, Text} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
 import MainNavigation from './navigation/MainNavigation';
 import {NavigationContainer} from '@react-navigation/native';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <MainNavigation />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
 
 import Header from '../../components/Header/Header';
 
 import globalStyle from '../../assets/styles/globalStyle';
 import style from './style';
-import Button from '../../components/Button/Button';
-import Tab from '../../components/Tab/Tab';
-import Badge from '../../components/Badge/Badge';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
-import Search from '../../components/Search/Search';
-import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
-import {horizontalScale} from '../../assets/styles/scaling';
+import {useDispatch, useSelector} from 'react-redux';
+import {updateFirstName} from '../../redux/reducers/User';
 
 const Home = () => {
+  const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      {/* <Search
-        onSearch={value => {
-          console.log(value);
-        }}
-      /> */}
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: horizontalScale(24),
+      <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable
+        onPress={() => {
+          dispatch(updateFirstName({firstName: 'S'}));
         }}>
-        <SingleDonationItem
-          uri={
-            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
-          }
-          badgeTitle={'Enviroment'}
-          donationTitle={'Tree Cactus'}
-          price={44}
-        />
-        <SingleDonationItem
-          uri={
-            'https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg'
-          }
-          badgeTitle={'Enviroment'}
-          donationTitle={'Tree Cactus'}
-          price={44}
-        />
-      </View>
+        <Text>Press me to change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
