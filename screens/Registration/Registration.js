b20e5a2311ec49791228521ab7067f27f314d239
Saeb Masarwa <saeb2312@gmail.com>
2024-10-22 10:27:48 +0300

    modified:   assets/styles/globalStyle.js 	new file:   components/Input/Input.js 	new file:   components/Input/style.js 	modified:   navigation/MainNavigation.js 	modified:   navigation/Routes.js 	new file:   screens/Login/Login.js 	new file:   screens/Login/style.js 	new file:   screens/Registration/Registration.js 	new file:   screens/Registration/style.js

---
+++ screens/Registration/Registration.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+
+import {Pressable, SafeAreaView, ScrollView, View} from 'react-native';
+
+import style2 from './style';
+import globalStyle from '../../assets/styles/globalStyle';
+import Input from '../../components/Input/Input';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
+import BackButton from '../../components/BackButton/BackButton';
+
+const Registration = ({navigation}) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <View style={style2.backButton}>
+        <BackButton onPress={() => navigation.goBack()} />
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={style2.container}>
+        <View style={globalStyle.marginBottom24}>
+          <Header type={1} title={'Hello'} />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            label={'First & Last Name'}
+            placeholder={'Enter your full name'}
+            onChangeText={value => setFullName(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            keyboardType={'email-address'}
+            label={'Email'}
+            placeholder={'Enter your email...'}
+            onChangeText={value => setEmail(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Input
+            secureTextEntry={true}
+            label={'Password'}
+            placeholder={'******'}
+            onChangeText={value => setPassword(value)}
+          />
+        </View>
+        <View style={globalStyle.marginBottom24}>
+          <Button title={'Registration'} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Registration;
